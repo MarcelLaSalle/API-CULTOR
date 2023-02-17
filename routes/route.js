d8ee1a9b991f7212679d5b3d0e228e032978c49a
MarcelLaSalle <115008364+MarcelLaSalle@users.noreply.github.com>
2023-02-17 17:04:51 +0100

    17/02

---
+++ routes/route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/controller');
+const controller2 = require('../controller/controller2');
+const middleware = require('../middleware/middleware');
+
+router.post('/sendContact', controller.nodemailerthing);
+router.post('/nodemailerNewPassword', controller2.nodemailerNewPassword);
+
+router.post('/register', controller2.crearRegistro);
+router.post('/loginWT', controller2.loginWT);
+router.post('/updateBBDDNewPassword', controller2.updateBBDDNewPassword);
+router.post('/recuperarcoontramamon',controller2.tokenRecuperacionPasswd);
+router.get('/protected', middleware.protectedMidd, controller2.tokenAcceso);
+
+module.exports = router;
